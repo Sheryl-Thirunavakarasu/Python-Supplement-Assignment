6bc126f5148ead5bc16e962ddb705e594dbe3a39
Sheryl-Thirunavakarasu <sherylt107@gmail.com>
2026-01-30 11:52:39 +0530

    Correct largest number calculation in problem_04.py
    Fix logic to find the largest number in the list.

--- problem_04.py
+++ problem_04.py
@@ -2,8 +2,9 @@
 # Find and fix the error
 
 numbers = [45, 12, 78, 34, 89, 23]
-largest = numbers[0]
-for i in range(len(numbers)):
-    if numbers[i] > largest:
-        largest = numbers[i]
+largest = 0
+for i in numbers:
+    if i > largest:
+        largest = i
+        
 print(f"Largest number is: {largest}")
